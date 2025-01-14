d18506c333daac8a7a74e6649c3eba36380734e5
chaimaa faris <chaimaafaris364@gmail.com>
2025-01-14 18:25:45 +0100

    Add font & colors &fontsize

--- app/src/main/java/com/example/managepass/MainActivity.kt
+++ app/src/main/java/com/example/managepass/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.managepass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Récupérer le bouton continue par son ID
+        val button = findViewById<Button>(R.id.ih);
+        // Ajouter un écouteur d'événements sur le bouton
+        button.setOnClickListener {
+            // Ouvrir l'activité SignInActivity
+            startActivity(Intent(this, SignInActivity::class.java))
         }
     }
 }

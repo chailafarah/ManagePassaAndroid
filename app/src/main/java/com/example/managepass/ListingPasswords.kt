2278d4a64f6c0bf46050a56a6daeb2f8bfdc9536
chaimaa faris <chaimaafaris364@gmail.com>
2025-01-25 21:13:03 +0100

    Page ListingPasswords

--- app/src/main/java/com/example/managepass/ListingPasswords.kt
+++ app/src/main/java/com/example/managepass/ListingPasswords.kt
@@ -1,12 +1,17 @@
 package com.example.managepass
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ListingPasswords : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,9 @@ class ListingPasswords : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //recureper les données de l'utilisateur nom et prenom
+        val user = Firebase.auth.currentUser
+        // set welcome_text (textview) to "Welcome $user.displayName"
+        findViewById<TextView>(R.id.welcome_text).text = "Welcome ${user!!.displayName}"
     }
 }
